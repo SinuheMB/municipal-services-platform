512419820c292aa34833178744188592270abb15
highsinuhe <highsinuhe@gmail.com>
2026-06-02 18:32:17 -0600

    feat: point frontend to Railway production API

--- frontend/src/api/client.ts
+++ frontend/src/api/client.ts
@@ -1,6 +1,7 @@
 import axios from 'axios'
 
-const API_URL = 'http://127.0.0.1:8000/api'
+// const API_URL = 'http://127.0.0.1:8000/api'
+const API_URL = 'https://web-production-24244.up.railway.app/api'
 
 const client = axios.create({
   baseURL: API_URL,
